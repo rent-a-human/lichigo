74521352e38880fbf9af2985383d6daba95e9085
John Lenin <jlenin@igm.technology>
2022-11-02 00:28:47 -0500

    Added Meta tag helper

--- app.js
+++ app.js
@@ -41,8 +41,11 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars helpers
+const { helpers } = require('./helpers/hbs')
+
 // Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs({ helpers: {helpers}, defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // Sessions
---
+++ helpers/hbs.js
@@ -0,0 +1,9 @@
+module.exports = {
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+}
